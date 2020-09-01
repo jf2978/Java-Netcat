b898d1958b171ee63fce54df467dad538c454c10
jeffrey-fabian <jfabian@rsglab.com>
2020-09-01 17:22:30 -0400

    update repo to use gradle

--- src/main/java/NetCat.java
+++ src/main/java/NetCat.java
@@ -1,46 +1,38 @@
 import java.io.*;
-
 import org.apache.commons.cli.*;
 
 public class NetCat {
 
-    /* ====
-        STATIC INNER CLASS - NetCatArgs
-    */
+    /*
+     * ==== STATIC INNER CLASS - NetCatArgs
+     */
     private static class NetCatArgs {
-        /* ====
-            VARIABLES
+        /*
+         * ==== VARIABLES
          */
         // Static (class) variables
-        private static final Option listen = new Option("l", "listen", false, "starts NetCat in listening mode (server)");
-        private static final Option hostname = Option.builder("h").
-                longOpt("hostname")
-                .valueSeparator('=')
-                .desc("hostname of the machine trying to connect to")
-                .hasArg()
-                .build();
-        private static final Option port = Option.builder("p").
-                longOpt("port")
-                .valueSeparator('=')
-                .desc("port number specifying the process to connect to (within the machine)")
-                .hasArg()
-                .build();
+        private static final Option listen = new Option("l", "listen", false,
+                "starts NetCat in listening mode (server)");
+        private static final Option hostname = Option.builder("h").longOpt("hostname").valueSeparator('=')
+                .desc("hostname of the machine trying to connect to").hasArg().build();
+        private static final Option port = Option.builder("p").longOpt("port").valueSeparator('=')
+                .desc("port number specifying the process to connect to (within the machine)").hasArg().build();
         private static final Options ncOptions = initOptions();
         private static final HelpFormatter help = new HelpFormatter();
 
         // Non-static (instance) variables
         private CommandLine cmd;
 
-        /* ====
-            CONSTRUCTOR(S)
+        /*
+         * ==== CONSTRUCTOR(S)
          */
         private NetCatArgs(String[] args) throws ParseException {
             CommandLineParser parser = new DefaultParser();
             cmd = parser.parse(ncOptions, args);
         }
 
-        /* ====
-            PUBLIC METHODS
+        /*
+         * ==== PUBLIC METHODS
          */
         // getHostname(): returns hostname if provided, else defaults to localhost
         private String getHostname() {
@@ -48,19 +40,21 @@ public class NetCat {
         }
 
         // getPort(): returns port if provided, else defaults to 8080
-        private int getPort() throws NumberFormatException{
+        private int getPort() throws NumberFormatException {
             return Integer.parseInt(cmd.getOptionValue("port", "8080"));
         }
 
-        // isServer(): checks if listen flag has been passed; used to branch into listening (server) mode or not
+        // isServer(): checks if listen flag has been passed; used to branch into
+        // listening (server) mode or not
         private boolean isServer() {
             return cmd.hasOption("listen");
         }
 
-        /* ====
-            PRIVATE METHODS
+        /*
+         * ==== PRIVATE METHODS
          */
-        // initOptions(): Prepares Options object for parsing by combining static Option objects
+        // initOptions(): Prepares Options object for parsing by combining static Option
+        // objects
         private static Options initOptions() {
             Options ncOptions = new Options();
             ncOptions.addOption(listen);
@@ -71,8 +65,8 @@ public class NetCat {
         }
     }
 
-    /* ====
-        MAIN METHOD
+    /*
+     * ==== MAIN METHOD
      */
     public static void main(String[] args) {
 
@@ -86,17 +80,17 @@ public class NetCat {
             ncArgs = new NetCatArgs(args);
             hostname = ncArgs.getHostname();
             port = ncArgs.getPort();
-        }catch (ParseException | NumberFormatException e) {
+        } catch (ParseException | NumberFormatException e) {
             e.printStackTrace();
             System.out.println(e.getMessage());
             System.exit(0);
         }
 
         // Start client/server
-        try(MySocketServer server = ncArgs.isServer() ? new MySocketServer(hostname, port): null;
-            MySocketClient client = server != null ? server.listen() : new MySocketClient(hostname, port);
-            NetCatProtocol nc = new NetCatProtocol(client.getInputStream(), client.getOutputStream());
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in))){
+        try (MySocketServer server = ncArgs.isServer() ? new MySocketServer(hostname, port) : null;
+                MySocketClient client = server != null ? server.listen() : new MySocketClient(hostname, port);
+                NetCatProtocol nc = new NetCatProtocol(client.getInputStream(), client.getOutputStream());
+                BufferedReader in = new BufferedReader(new InputStreamReader(System.in))) {
 
             System.out.println("*** You are now connected to NetCat 1.0 ***\n");
 
@@ -104,15 +98,16 @@ public class NetCat {
             Thread recv = new Thread(nc);
             recv.start();
 
-            // Continuously read from stdin (new line delimiter implicit through BufferedReader use)
+            // Continuously read from stdin (new line delimiter implicit through
+            // BufferedReader use)
             String inputLine;
-            while ((inputLine = in.readLine()) != null){
-                if(inputLine.equals("exit")) {
+            while ((inputLine = in.readLine()) != null) {
+                if (inputLine.equals("exit")) {
                     System.out.println("-   Exiting NetCat   -");
                     System.exit(0);
                 }
 
-                nc.send(inputLine);
+                nc.send(inputLine + '\n');
             }
         } catch (NumberFormatException | IOException e) {
             System.out.println(e.getMessage());
